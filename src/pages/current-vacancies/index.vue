a67c23b8fe16809c9c19e64a682bb3ccf5c836b5
aNesterovic <anest1995@ukr.net>
2023-11-22 23:21:32 +0200

    added few changes in table

--- src/pages/current-vacancies/index.vue
+++ src/pages/current-vacancies/index.vue
@@ -15,57 +15,102 @@
         <a-input-search
           v-model:value="searchValue"
           placeholder="Введите текст"
-          style="width: 250px"
+          style="width: 20%"
           @search="onSearch"
         />
-        <a-button class="vacancies__table-button" type="primary" :size="size">
+        <a-button :disabled="addNewVacancy" class="vacancies__table-button" type="primary" v-if="!selectedRows.length" @click="addNewVacancy = true">
           <template #icon>
             <PlusOutlined />
           </template>
           Добавить новую
         </a-button>
-      </div>
-      <a-table :columns="columns" :data-source="data" :row-selection="rowSelection">
-      <template #bodyCell="{ column, text }">
-        <template v-if="column.dataIndex === 'company'">
-          <a @click.prevent="() => console.log('link')">{{ text }}</a>
-        </template>
-        <template v-if="column.dataIndex === 'vessel'">
-          <a @click.prevent="() => console.log('link')">{{ text }}</a>
-        </template>
-        <template v-if="column.dataIndex === 'joining'">
-          <a-tag v-if="text === 'Asap'" color="red">{{ text }}</a-tag>
-        </template>
-        <template v-if="column.dataIndex === 'posting' && !text">
-          <a-button class="vacancies__table-button" type="primary" shape="circle">
+        <div class="vacancies__table-selected" v-else>
+          <a-button :style="{color: '#22A1FC'}">
+            Удалить
+          </a-button>
+          <a-button :style="{color: '#22A1FC'}">
+            Назначить ответственного
+          </a-button>
+          <a-button :style="{color: '#fff', background: '#22A1FC'}">
             <template #icon>
               <SendOutlined />
             </template>
+            Опубликовать выбранное
           </a-button>
+        </div>  
+      </div>
+      <a-table bordered sticky :pagination="false" :columns="columns" :data-source="data" :row-selection="rowSelection" :row-class-name="(_record, index) => tableRowHandle(_record)">
+        <template #summary v-if="addNewVacancy">
+          <a-table-summary fixed="top">
+            <a-table-summary-row>
+              <a-table-summary-cell :index="0">
+              </a-table-summary-cell>
+              <a-table-summary-cell :index="1"><a-input size="small" v-model:value="formAddNewVacancy.company" placeholder="Company" /></a-table-summary-cell>
+              <a-table-summary-cell :index="2"><a-input size="small" v-model:value="formAddNewVacancy.position" placeholder="Position" /></a-table-summary-cell>
+              <a-table-summary-cell :index="3"><a-input size="small" v-model:value="formAddNewVacancy.vessel" placeholder="Vessel" /></a-table-summary-cell>
+              <a-table-summary-cell :index="4"><a-date-picker size="small" placeholder="Joining" v-model:value="joining" /></a-table-summary-cell>
+              <a-table-summary-cell :index="5"><a-input size="small" v-model:value="formAddNewVacancy.wage" placeholder="Wage" /></a-table-summary-cell>
+              <a-table-summary-cell :index="6"><a-input size="small" v-model:value="formAddNewVacancy.doc" placeholder="DOC" /></a-table-summary-cell>
+              <a-table-summary-cell :index="7"><a-input size="small" v-model:value="formAddNewVacancy.remark" placeholder="" /></a-table-summary-cell>
+              <a-table-summary-cell :index="8">
+                <a-button size="small" shape="circle" :style="{color: '#22A1FC'}">
+                  <template #icon>
+                    <UserAddOutlined />
+                  </template>
+                </a-button>
+              </a-table-summary-cell>
+              <a-table-summary-cell :index="9"></a-table-summary-cell>
+              <a-table-summary-cell :index="10">
+                <a-button size="small" shape="round" :style="{color: '#fff', background: '#22A1FC'}" @click="createNewVacancy">
+                  Сохранить
+                </a-button>
+                <a-button size="small" shape="circle" @click="removeNewForm">
+                  <template #icon>
+                    <CloseOutlined :style="{color: '#8C8C8C'}" />
+                  </template>
+                </a-button>
+              </a-table-summary-cell>
+            </a-table-summary-row>
+          </a-table-summary>
         </template>
-        <template v-if="column.dataIndex === 'actions'" class="vacancies__table-actions">
-          <a-button shape="circle" class="vacancies__table-button_action" v-if="text.idcardOutlined">
-            <template #icon>
-              <IdcardOutlined />
-            </template>
-          </a-button>
-          <a-button shape="circle" v-else>
-            <template #icon>
-              <IdcardOutlined />
-            </template>
-          </a-button>
-          <a-button shape="circle" class="vacancies__table-button_action" v-if="text.fireOutlined">
-            <template #icon>
-              <FireOutlined />
-            </template>
-          </a-button>
-          <a-button shape="circle" v-else>
-            <template #icon>
-              <FireOutlined />
-            </template>
-          </a-button>
+        <template #bodyCell="{ column, text }" :class="{'vacancies__table-oulined': text.idcardOutlined}">
+          <template v-if="column.dataIndex === 'company'">
+            <a @click.prevent="() => console.log('link')">{{ text }}</a>
+          </template>
+          <template v-if="column.dataIndex === 'vessel'">
+            <a @click.prevent="() => console.log('link')">{{ text }}</a>
+          </template>
+          <template v-if="column.dataIndex === 'assignee' && text">
+            <a-avatar style="background-color: #f56a00">{{ text[0] }}</a-avatar>
+          </template>
+          <template v-if="column.dataIndex === 'joining'">
+            <a-tag color="pink" v-if="text === 'Asap'">{{ text }}</a-tag>
+          </template>
+          <template v-if="column.dataIndex === 'posting' && !text">
+            <a-button class="vacancies__table-button" type="primary" shape="circle">
+              <template #icon>
+                <SendOutlined />
+              </template>
+            </a-button>
+          </template>
+          <template v-if="column.dataIndex === 'actions'" class="vacancies__table-actions">
+            <a-button shape="circle" :class="{'vacancies__table-button_action': text.idcardOutlined}" @click="text.idcardOutlined = !text.idcardOutlined">
+              <template #icon>
+                <IdcardOutlined :style="{color: '#8C8C8C'}" />
+              </template>
+            </a-button>
+            <a-button  shape="circle" :class="{'vacancies__table-button_action': text.fireOutlined}" @click="text.fireOutlined = !text.fireOutlined">
+              <template #icon>
+                <FireOutlined :style="{color: '#8C8C8C'}" />
+              </template>
+            </a-button>
+            <a-button shape="circle" type="text">
+              <template #icon>
+                <MoreOutlined :style="{color: '#22A1FC'}" />
+              </template>
+            </a-button>
+          </template>
         </template>
-      </template>
       </a-table>
     </div>
   </div>
@@ -82,72 +127,135 @@ export default {
           title: 'Company',
           dataIndex: 'company',
           key: 'company',
+          filters: []
         },
         {
           title: 'Position',
           dataIndex: 'position',
           key: 'position',
+          filters: []
         },
         {
           title: 'Vessel',
           dataIndex: 'vessel',
           key: 'vessel',
+          filters: [],
+          width: '13%'
         },
         {
           title: 'Joining',
           dataIndex: 'joining',
           key: 'joining',
+          filters: [],
+          width: '10%'
         },
         {
           title: 'Wage',
           dataIndex: 'wage',
           key: 'wage',
+          filters: []
         },
         {
           title: 'DOC',
           dataIndex: 'doc',
           key: 'doc',
+          filters: []
         },
         {
           title: 'Remark',
           dataIndex: 'remark',
           key: 'remark',
+          filters: []
         },
         {
           title: 'Assignee',
           dataIndex: 'assignee',
           key: 'assignee',
+          filters: []
         },
         {
           title: 'Posting',
           dataIndex: 'posting',
           key: 'posting',
+          filters: []
         },
         {
           title: '',
           dataIndex: 'actions',
           key: 'actions',
-          width: '120px'
         },
       ]);
       const data = ref(mockTable);
+      const addNewVacancy = ref(false)
+      const selectedRows = ref([]);
+      const formAddNewVacancy = ref({
+        company: '',
+        position: '',
+        vessel: '',
+        joining: '',
+        wage: '',
+        doc: '',
+        remark: '',
+        assignee: [],
+      })
       const rowSelection = ref({
         checkStrictly: false,
-        onChange: (selectedRowKeys, selectedRows) => {
-          console.log(`selectedRowKeys: ${selectedRowKeys}`, 'selectedRows: ', selectedRows);
-        },
-        onSelect: (record, selected, selectedRows) => {
-          console.log(record, selected, selectedRows);
-        },
-        onSelectAll: (selected, selectedRows, changeRows) => {
-          console.log(selected, selectedRows, changeRows);
-        },
+        onChange: (selectedRowKeys, selected) => {
+          selectedRows.value = selected
+        }
       });
+
+      const removeNewForm = () => {
+        addNewVacancy.value = false
+        formAddNewVacancy.value = {
+          company: '',
+          position: '',
+          vessel: '',
+          joining: '',
+          wage: '',
+          doc: '',
+          remark: '',
+          assignee: [],
+        }
+      }
+
+      const createNewVacancy = () => {
+        addNewVacancy.value = false
+        formAddNewVacancy.value = {
+          company: '',
+          position: '',
+          vessel: '',
+          joining: '',
+          wage: '',
+          doc: '',
+          remark: '',
+          assignee: [],
+        }
+      }
+
+      const tableRowHandle = (record) => {
+        const styleArray = []
+        if (record.actions.idcardOutlined && record.actions.fireOutlined) {
+          styleArray.push('vacancies__table-both')
+        } else if (record.actions.fireOutlined) {
+          styleArray.push('vacancies__table-oulined')
+        } else if (record.actions.idcardOutlined) {
+          styleArray.push('vacancies__table-idcard')
+        }
+
+        return styleArray.join(' ')
+      }
 
       return {
         rowSelection,
         data,
-        columns
+        columns,
+        addNewVacancy,
+        tableRowHandle,
+        selectedRows,
+        removeNewForm,
+        createNewVacancy,
+        formAddNewVacancy
       }
   }
 }
@@ -181,11 +289,26 @@ export default {
   }
 
   &__table {
-    padding: 0px 25px;
+    padding: 0px 25px 35px 25px;
+
+    &-idcard {
+      background-color: #E6FFFB;
+    }
+    &-oulined {
+      background-color: #FFFBE6;
+    }
+    &-both {
+      background-color: #F6FFED;
+    }
     &-toolbar {
       display: flex;
       justify-content: space-between;
       padding: 16px 8px;
+    }
+    &-selected {
+      display: flex;
+      justify-content: flex-end;
+      gap: 8px;
     }
     &-button {
       width: 165px;
@@ -194,10 +317,28 @@ export default {
         background: #2EE9A8;
       }
     }
-    &-actions {
-      display: flex;
-      width: 100%;
-      gap: 15px;
+
+    .ant-table-cell {
+      font-family: 'Roboto', sans-serif;
+      padding: 4px 8px !important;
+    }
+    .ant-table-tbody {
+      .ant-table-cell:last-child {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+      }
+      .ant-table-cell:nth-child(5) {
+        font-size: 12px;
+      }
+    }
+
+    .ant-table-summary {
+      .ant-table-cell:last-child {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+      }
     }
 
     .ant-table-row {
